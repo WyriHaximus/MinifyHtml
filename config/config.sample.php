150321a7ceb337d738411404a31d26bfd95169f3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-04-05 18:22:22 +0200

    Added comments for the config values

--- config/config.sample.php
+++ config/config.sample.php
@@ -3,8 +3,8 @@
 return [
     'WyriHaximus' => [
         'MinifyHtml' => [
-            'debugOverride' => false,
-            'factory' => 'WyriHaximus\HtmlCompress\Factory::constructFastest',
+            'debugOverride' => false, // Minify even when debug is enabled
+            'factory' => 'WyriHaximus\HtmlCompress\Factory::constructFastest', // Parser factory
         ],
     ],
 ];
